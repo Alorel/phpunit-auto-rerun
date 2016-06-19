48a4601bba8b32cf6d581dce3ac7a15173bcf20b
Art <a.molcanovas@gmail.com>
2016-06-19 21:00:46 +0100

    codecov test

--- test/Runtime/RuntimeTest.php
+++ test/Runtime/RuntimeTest.php
@@ -8,9 +8,7 @@
     class RuntimeTest extends \PHPUnit_Framework_TestCase {
 
         function testException() {
-            exec(RUNTIME_BOOTSTRAP . (new RC(ExceptionThrower::class))->getFileName(),
-                 $out,
-                 $ret);
+            exec(RUNTIME_BOOTSTRAP . (new RC(ExceptionThrower::class))->getFileName(), $out, $ret);
 
             $this->assertContains('OK (1 test, 3 assertions)', $out);
             $this->assertEquals(0, $ret);
