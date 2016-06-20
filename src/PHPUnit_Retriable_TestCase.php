1379e123e1197f314aa78888dd3534e2c4e9db6a
Art <a.molcanovas@gmail.com>
2016-06-20 23:11:58 +0100

    -some- PHPDoc. unfinished.

--- src/PHPUnit_Retriable_TestCase.php
+++ src/PHPUnit_Retriable_TestCase.php
@@ -3,15 +3,56 @@
     use Alorel\PHPUnitRetryRunner\Annotations\DocBlockFactoryManager;
     use Alorel\PHPUnitRetryRunner\Annotations\Tags\RetryCount;
     use Alorel\PHPUnitRetryRunner\Annotations\Tags\SleepTime;
+    use Alorel\PHPUnitRetryRunner\PHPUnitReflection;
 
+    /**
+     * Automatically reruns a failing test case up to n times. See link for full configuration instructions and
+     * examples.
+     *
+     * @author Art <a.molcanovas@gmail.com>
+     * @see    https://github.com/Alorel/phpunit-auto-rerun/#configuration
+     */
     class PHPUnit_Retriable_TestCase extends PHPUnit_Framework_TestCase {
 
+        /**
+         * How many seconds to sleep between retries
+         *
+         * @var int
+         */
         private $sleepTime = 0;
 
+        /**
+         * Maximum number of test attempts. 0 simply invokes the standard test runner, 1 runs a test case once
+         * without retries, >=2 will apply retries on failure.
+         *
+         * @var int
+         */
         private $retryCount = 0;
 
+        /**
+         * A {@link ReflectionClass} of $this
+         *
+         * @var ReflectionClass
+         */
         private $thisReflect;
 
+        /**
+         * Message shown on retries
+         *
+         * @var string
+         */
+        private static $retryMsg = '%s failed; waiting for %d seconds before retry %d/%d' . PHP_EOL;
+
+        /**
+         * PHPUnit_Retriable_TestCase constructor. Receives the same arguments as
+         * {@link PHPUnit_Framework_TestCase::__construct() the standard PHPUnit constructor}.
+         *
+         * @author Art <a.molcanovas@gmail.com>
+         *
+         * @param string $name
+         * @param array  $data
+         * @param string $dataName
+         */
         function __construct($name = null, array $data = [], $dataName = '') {
             $this->thisReflect = new ReflectionClass($this);
 
@@ -29,6 +70,14 @@
             parent::__construct($name, $data, $dataName);
         }
 
+        /**
+         * Parses the sleepTime and retryCount annotations for the given method
+         *
+         * @author Art <a.molcanovas@gmail.com>
+         *
+         * @param string $sleepTime  Reference to the variable which will hold the computed sleep time
+         * @param string $retryCount Reference to the variable which will hold the computed retry count
+         */
         private function parseMethodAnnotations(&$sleepTime, &$retryCount) {
             $sleepTime = $this->sleepTime;
             $retryCount = $this->retryCount;
@@ -46,8 +95,12 @@
         }
 
         /**
+         * Runs the bare test sequence.
+         *
+         * @author             Art <a.molcanovas@gmail.com>
          * @throws Exception
          * @codeCoverageIgnore Won't be picked up as these are tested in separate processes
+         * @uses               PHPUnit_Framework_TestCase::runBare()
          */
         public function runBare() {
             $this->parseMethodAnnotations($sleepTime, $retryCount);
@@ -66,12 +119,13 @@
                         if ($numRuns == $retryCount) {
                             throw $e;
                         } else {
-                            \Alorel\PHPUnitRetryRunner\PHPUnitReflection::getMessage()->setValue($this, '');
-                            \Alorel\PHPUnitRetryRunner\PHPUnitReflection::getStatus()->setValue($this, null);
+                            PHPUnitReflection::getMessage()->setValue($this, '');
+                            PHPUnitReflection::getStatus()->setValue($this, null);
 
-                            fwrite(STDERR,
-                                   $this->getName(false) . ' failed; waiting for ' . $sleepTime
-                                   . ' seconds before retry # ' . $numRuns . '/' . $retryCount . PHP_EOL);
+                            fwrite(
+                                STDERR,
+                                sprintf(self::$retryMsg, $this->getName(false), $sleepTime, $numRuns, $retryCount)
+                            );
 
                             sleep($sleepTime);
                         }
